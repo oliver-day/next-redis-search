9338048d77005c3055725e02bc9a6d65eac6845b
Oliver Day <oliver.day.swe@gmail.com>
2023-06-24 11:31:41 -0700

    feat: add api '/search' route

---
+++ pages/api/search.js
@@ -0,0 +1,8 @@
+import { searchCars } from "@/lib/redis";
+
+export default async function handler(req, res) {
+  const { query } = req.query;
+
+  const cars = await searchCars(query);
+  res.status(200).json({ cars });
+};
